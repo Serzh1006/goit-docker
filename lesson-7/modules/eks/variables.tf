96afc760b566dcacd254db6b31e45191db629e9c
Sergey Kravchenko <skravchenko223@gmail.com>
2026-02-10 17:21:33 +0100

    hw is done

---
+++ lesson-7/modules/eks/variables.tf
@@ -0,0 +1,14 @@
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID for EKS cluster"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Subnets for EKS cluster"
+  type        = list(string)
+}
